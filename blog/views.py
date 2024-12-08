738ee7dc7a4b5aed77b153d0b4887c338fca735d
shahramsamar <shahramsamar2010@gmail.com>
2024-12-08 13:56:29 +0330

    added ordering

--- blog/views.py
+++ blog/views.py
@@ -7,13 +7,20 @@ from blog import models
 
 class PostListView(ListView):
     # three way use  this  methods
-    # model = models.Post
+    
+    model = models.Post
+    
     # queryset = models.Post.objects.all()
-    def get_queryset(self):
-         posts =models.Post.objects.all()
-         return posts
+    
+    # def get_queryset(self):
+    #      posts =models.Post.objects.all()
+    #      return posts
      
     # if not write this  method ,i must use name object_list 
     context_object_name = 'posts'
+    
     # for example if you have a posts object
     paginate_by = 1
+    
+    # when i use ordering, i dont use get_queryset
+    ordering = '-id'
