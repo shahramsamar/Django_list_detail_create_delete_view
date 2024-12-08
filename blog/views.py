c7b87c98100d6c487f299db01e6356ef27142d27
shahramsamar <shahramsamar2010@gmail.com>
2024-12-08 13:39:40 +0330

    create  model post in blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from blog import models
 
-# Create your views here.
+
+
+
+class PostListView(ListView):
+    # three way use  this  methods
+    # model = models.Post
+    # queryset = models.Post.objects.all()
+    def get_queryset(self):
+         posts =models.Post.objects.all()
+         return posts
+     
+    # if not write this  method ,i must use name object_list 
+    context_object_name = 'posts'
